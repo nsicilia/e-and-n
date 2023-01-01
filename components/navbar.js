760bc8d1f0cc49ec2da991da32ecb42df86b3a31
Nicholas Siciliano <nsicilia@oswego.edu>
2022-12-31 19:20:34 -0500

    Changed the logo

--- components/navbar.js
+++ components/navbar.js
@@ -20,7 +20,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
           <Link href="/">
             <Image
-              src="/EandNlogo.svg"
+              src="/ENlogo.png"
               width={360}
               height={80}
               alt="Float UI logo"
