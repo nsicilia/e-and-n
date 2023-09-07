a95874d9c59d0e078877f1e8dcd5a0a911ccc25c
Nicholas Siciliano <nsicilia@oswego.edu>
2023-09-06 21:02:47 -0400

    fixes

--- components/aboutus.js
+++ components/aboutus.js
@@ -60,29 +60,35 @@ export const AboutUs = ({ title, body, img1, img2, img3 }) => {
         </div>
         <div className="flex items-center justify-center -mx-4 lg:pl-8">
           <div className="flex flex-col items-end px-3">
-            <Image
-              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src={urlFor(img1).url()}
-              alt=""
-              width={223}
-              height={224}
-            />
-            <Image
-              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src={urlFor(img3).url()}
-              alt=""
-              width={160}
-              height={160}
-            />
+            {img1 && (
+              <Image
+                className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+                src={urlFor(img1).url()}
+                alt=""
+                width={223}
+                height={224}
+              />
+            )}
+            {img3 && (
+              <Image
+                className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+                src={urlFor(img3).url()}
+                alt=""
+                width={160}
+                height={160}
+              />
+            )}
           </div>
           <div className="px-3">
-            <Image
-              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src={urlFor(img2).url()}
-              alt=""
-              width={318}
-              height={320}
-            />
+            {img2 && (
+              <Image
+                className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+                src={urlFor(img2).url()}
+                alt=""
+                width={318}
+                height={320}
+              />
+            )}
           </div>
         </div>
       </div>
